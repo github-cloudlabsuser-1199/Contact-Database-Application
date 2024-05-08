e511642ba522bff55222918b108daff1139abd6d
github_cloudlabsuser_1199@cloudlabsaiuser.com <github_cloudlabsuser_1199@cloudlabsaiuser.com>
2024-05-08 17:55:13 +0000

    runing app

---
+++ Controllers/MyDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+public class MyDbContext : DbContext
+{
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> AppUsers { get; set; }
+}
